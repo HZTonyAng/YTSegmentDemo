e1f5913accfe964938008c3fe4c53a072fdaf030
TonyAng <13323650885@163.com>
2018-04-25 13:14:08 +0800

    Initial Commit

---
+++ YTSegmentDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YTSegmentDemo
+//
+//  Created by TonyAng on 2018/4/25.
+//  Copyright © 2018年 TonyAng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YTSegmentDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YTSegmentDemo
+//
+//  Created by TonyAng on 2018/4/25.
+//  Copyright © 2018年 TonyAng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
